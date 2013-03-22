c3d69a3c5324e02df1b8cca493bd5224b861f9d2
thiamshui <thiamshui@gmail.com>
2013-03-22 13:07:00 +0000

    videoview player

---
+++ LondonEntrepreneurship/src/com/londonentrepreneurshiponline/VideoActivity.java
@@ -0,0 +1,33 @@
+package com.londonentrepreneurshiponline;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class VideoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_video);
+		VideoView vid = (VideoView) findViewById(R.id.videoView1);
+		MediaController mc = new MediaController(this);
+		mc.setAnchorView(vid);
+		mc.setMediaPlayer(vid);
+		vid.setVideoURI(Uri.parse("http://www.londonentrepreneurshiponline.com/stream/yqqgv4v0snfohqsj.mp4"));
+		vid.setMediaController(mc);
+		
+		vid.start();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.video, menu);
+		return true;
+	}
+
+}
