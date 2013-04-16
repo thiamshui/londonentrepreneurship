8d35fc911f70118a9a2aa410020c767701383f40
thiamshui <thiamshui@gmail.com>
2013-04-16 14:04:46 +0100

    added video search functions/featured/latest videos

--- LondonEntrepreneurship/src/com/londonentrepreneurshiponline/models/Video.java
+++ LondonEntrepreneurship/src/com/londonentrepreneurshiponline/models/Video.java
@@ -81,18 +81,18 @@ public class Video {
 	/* deserialize video collection
 	 * Reference: https://sites.google.com/site/gson/gson-user-guide#TOC-Collections-Examples
 	 */
-	public static ArrayList<Video> getAllVideos()
+	public static ArrayList<Video> getFeaturedVideos()
 	{
-		GsonBuilder gsonb = new GsonBuilder();
-		Gson gson = gsonb.create();
-
-		Type vidCollection = new TypeToken<ArrayList<Video>>() {}.getType();
-		ArrayList<Video> videos = null;
-
-		String json = WSClient.httpGET("http://comp1008.thiamshui.net/video.php");
-		videos = gson.fromJson(json, vidCollection);
-		
-		return videos;
+		return getVideosByUrl("http://comp1008.thiamshui.net/video.php?featured");
+	}
+	public static ArrayList<Video> getLatestVideos()
+	{
+		return getVideosByUrl("http://comp1008.thiamshui.net/video.php?latest");
+	}
+	
+	public static ArrayList<Video> searchVideos(String searchStr)
+	{
+		return getVideosByUrl("http://comp1008.thiamshui.net/video.php?search=" + searchStr);
 	}
 
 	public static Video getVideoById(int id) {
@@ -101,7 +101,20 @@ public class Video {
 		Gson gson = gsonb.create();
 
 		jsonString = WSClient.httpGET("http://comp1008.thiamshui.net/video.php?id="	+ id);
-		//Log.d("test",jsonString);
 		return (Video) gson.fromJson(jsonString, Video.class);
+	}
+	
+	private static ArrayList<Video> getVideosByUrl(String url)
+	{
+		GsonBuilder gsonb = new GsonBuilder();
+		Gson gson = gsonb.create();
+
+		Type vidCollection = new TypeToken<ArrayList<Video>>() {}.getType();
+		ArrayList<Video> videos = null;
+
+		String json = WSClient.httpGET(url);
+		videos = gson.fromJson(json, vidCollection);
+		
+		return videos;
 	}
 }
