4b9c673acc1354346e6f6a178cf648726f79c04a
Nilesh <Nilesh@128.86.185.98>
2013-04-18 20:55:30 +0100

    last

--- LondonEntrepreneurship/src/com/londonentrepreneurshiponline/annotate/AnnotateTextActivity.java
+++ LondonEntrepreneurship/src/com/londonentrepreneurshiponline/annotate/AnnotateTextActivity.java
@@ -3,17 +3,44 @@ package com.londonentrepreneurshiponline.annotate;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.londonentrepreneurshiponline.R;
 
 public class AnnotateTextActivity extends Activity {
-
+	
+	Button saving, discarding;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_annotate_text);
+		
+		saving = (Button) findViewById(R.id.Bsave);
+		saving.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				EditText saveAnnot = (EditText) findViewById(R.id.textAnnot);
+				String newAnnot = saveAnnot.getText().toString();
+			}
+		});
+		
+		discarding = (Button) findViewById(R.id.Bdiscard);
+		discarding.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				finish();
+			}
+		});
 	}
-
+ 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
