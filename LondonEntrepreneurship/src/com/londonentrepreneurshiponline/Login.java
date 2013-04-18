2d9c773d088bc45f56c5969af125cd87de3c9f25
Nilesh <Nilesh@128.86.185.98>
2013-04-18 12:01:18 +0100

    login

--- LondonEntrepreneurship/src/com/londonentrepreneurshiponline/Login.java
+++ LondonEntrepreneurship/src/com/londonentrepreneurshiponline/Login.java
@@ -1,17 +1,38 @@
 package com.londonentrepreneurshiponline;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class Login extends Activity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		String x = "nilesh";
+		md5(x);
 	}
 
+			public static String md5(String s) {
+			    try {
+			        // Create MD5 Hash
+			        MessageDigest digest = java.security.MessageDigest
+			                .getInstance("MD5");
+			        digest.update(s.getBytes(), 0, s.length());
+			        
+			        String y = new BigInteger(1, digest.digest()).toString(16);
+			    } catch (NoSuchAlgorithmException e) {
+			        e.printStackTrace();
+			    }
+			    return "";
+			}
+		
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
